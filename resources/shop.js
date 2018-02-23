03470d7d00dd1338fc7d6156211feb592f716fd9
Thielith <aydensgame@gmail.com>
2018-02-23 10:48:30 -0800

    a

--- resources/shop.js
+++ resources/shop.js
@@ -20,7 +20,7 @@ socket.emit(
 );
 
 socket.on('convert', function(name){
-	var displayString = []
+	var displayString = who + "ItemsDisplay"
 	var string = who + "Items"
 	var displayVariable;
 	var variable;
@@ -49,7 +49,7 @@ socket.on('convert', function(name){
 		for(r = 0; r < variable.length; r++){
 			var t = document.getElementById(string).innerHTML =
 				document.getElementById(string).innerHTML
-				+ "<p id='" + variable + r + "' onclick='buy(" + r + ")'>" + displayString[r] + "</p>";
+				+ "<p id='" + variable + r + "' onclick='buy(" + r + ")'>" + displayVariable[r] + "</p>";
 		}
 	}
 	
