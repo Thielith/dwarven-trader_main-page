472352dfe1028e3e80d7a482c5f8fffbaae458f5
Thielith <aydensgame@gmail.com>
2018-03-08 10:29:42 -0800

    First code cleanup test

--- resources/shop.js
+++ resources/shop.js
@@ -4,14 +4,21 @@ var playerItemsDisplay = [];
 var playerID = 0
 var playerItemsPrice = [];
 
+//playerID, playerMoney, {Item, Item Display, Price}
+var player = [0, 29, {ItemID: 5, ItemName: undefined, Price: undefined}, {ItemID: 6, ItemName: undefined, Price: undefined}]
+
 var shopItems = [1, 2, 3, 4];
 var shopItemsDisplay = [];
 var shopID = -1
 var shopItemsPrice = [];
 
+//shopID, {Item, Item Display, Price}
+var shop = [-1, {ItemID: 1, ItemName: "asdf", Price: undefined}, {ItemID: 2, ItemName: "no", Price: undefined}, {ItemID: 3, ItemName: undefined, Price: undefined}, {ItemID: 4, ItemName: undefined, Price: undefined}]
+
+
 var socket = io.connect('http://10.0.2.15:33336');
 
-var r = 0;
+var r = 1;
 var rr = 0;
 var buyNum = 0;
 var sellNum = 0;
@@ -21,65 +28,71 @@ var send = []
 
 document.getElementById('playerMoney').innerHTML = playerMoney
 
-socket.emit(
-	'convert', shopItems[r]
-);
+function getData(){
+	//For when theres a table for player info
+	//player.push({})
+	//player[i].ItemID = 90
+	//console.log(player[i])
+}
 
+socket.emit(
+	'convert', shop[r].ItemID
+);
 
 socket.on('convert', function(list){
 	if(who == "shop"){
-		shopItemsDisplay[r] = list[0]
-		shopItemsPrice[r] = list[1]
+		shop[r].ItemName = list[0]
+		shop[r].Price = list[1]
 		r += 1
 		
-		if(r != shopItems.length){
+		if(r != shop.length - 1){
 			socket.emit(
-				'convert', shopItems[r]
+				'convert', shop[r].ItemID
 			);
 		}
 		
 		else{
-			for(rr = 0; rr < shopItems.length; rr++){
+			for(rr = 1; rr < shop.length; rr++){
 				var t = document.getElementById('shopItems').innerHTML =
 					document.getElementById('shopItems').innerHTML
-					+ "<p id='shopItems" + rr + "' onclick='buy(" + rr + ")'>" + shopItemsDisplay[rr] + "</p>";
+					+ "<p id='shopItems" + rr + "' onclick='buy(" + rr + ")'>" + shop[rr].ItemName + "</p>";
 				
 				var tt = document.getElementById('shopItems').innerHTML =
 					document.getElementById('shopItems').innerHTML
-					+ "<p id='shopItems" + rr + "a'> ^" + shopItemsPrice[rr] + " Gold^</p>";
+					+ "<p id='shopItems" + rr + "a'> ^" + shop[rr].Price + " Gold^</p>";
 				buyNum = rr + 1
 			}
 			
 			who = "player"
-			r = 0
+			r = 2
 			rr = 0
 			socket.emit(
-				'convert', playerItems[r]
+				'convert', player[r].ItemID
 			);
 		}
 
 	}
 
 	else if(who == "player"){
-		playerItemsDisplay[r] = list[0]
-		playerItemsPrice[r] = list[1]
+		player[r].ItemName = list[0]
+		player[r].Price = list[1]
 		r += 1
 		
-		if(r != playerItems.length){
+		if(r != player.length){
 			socket.emit(
-				'convert', playerItems[r]
+				'convert', player[r].ItemID
 			);
 		}
 		
 		else{
-			for(rr = 0; rr < playerItems.length; rr++){
+			for(rr = 2; rr < player.length; rr++){
 				var t = document.getElementById('playerItems').innerHTML =
 					document.getElementById('playerItems').innerHTML
-					+ "<p id='playerItems" + rr + "' onclick='sell(" + rr + ")'>" + playerItemsDisplay[rr] + "</p>";
+					+ "<p id='playerItems" + rr + "' onclick='sell(" + rr + ")'>" + player[rr].ItemName + "</p>";
 				
 				var tt = document.getElementById('playerItems').innerHTML =
 					document.getElementById('playerItems').innerHTML
-					+ "<p id='playerItems" + rr + "a'> ^" + playerItemsPrice[rr] + " Gold^</p>";
+					+ "<p id='playerItems" + rr + "a'> ^" + player[rr].Price + " Gold^</p>";
 				sellNum = rr + 1
 			}
 		}
@@ -108,11 +121,9 @@ function recordTransfer(BuyerID, SellerID, ResourceID, Quantity, Price){
 
 function buy(num){
 	console.log("buy")
-	var item = document.getElementById('shopItems' + num).innerHTML
-	var index = shopItemsDisplay.indexOf(item)
-	console.log(shopItemsPrice)
+
 	
-	if(shopItemsPrice[index] > playerMoney){
+	if(shop[num].Price > playerMoney){
 		console.log("Too Expensive")
 		var ytv = document.getElementById('shopItems' + num + "a").innerHTML
 		document.getElementById('shopItems' + num + "a").innerHTML = "Too Expensive"
@@ -122,59 +133,59 @@ function buy(num){
 	}
 	
 	else{
-		playerMoney -= parseInt(shopItemsPrice[index])
+		playerMoney -= parseInt(shop[num].Price)
 		document.getElementById('playerMoney').innerHTML = playerMoney
 		
 		document.getElementById('playerItems').innerHTML =
 			document.getElementById('playerItems').innerHTML
-			+ "<p id='playerItems" + sellNum + "' onclick='sell(" + sellNum + ")'>" + shopItemsDisplay[num] + "</p>";
+			+ "<p id='playerItems" + sellNum + "' onclick='sell(" + sellNum + ")'>" + shop[num].ItemName + "</p>";
 		
 		document.getElementById('playerItems').innerHTML =
 			document.getElementById('playerItems').innerHTML
-			+ "<p id='playerItems" + sellNum + "a'> ^" + shopItemsPrice[num] + " Gold^</p>";
+			+ "<p id='playerItems" + sellNum + "a'> ^" + shop[num].Price + " Gold^</p>";
 		
-		playerItems.push(shopItems[num])
-		playerItemsDisplay.push(shopItemsDisplay[num])
-		playerItemsPrice.push(shopItemsPrice[num])
+		player.push({})
+		player[player.length].ItemID = shop[num].ItemID
+		player[player.length].ItemName = shop[num].ItemName
+		player[player.length].Price = shop[num].Price
 		
 		document.getElementById('shopItems' + num).innerHTML = ""
 		document.getElementById('shopItems' + num + "a").innerHTML = ""
 		sellNum += 1
 		
-		recordTransfer(playerID, shopID, shopItems[num], 1, shopItemsPrice[num])
-		shopItems[index] = ""
-		shopItemsDisplay[index] = ""
-		shopItemsPrice[index] = ""
+		recordTransfer(playerID, shopID, shop[num].ItemID, 1, shop[num].Price)
+		shopItems[num] = ""
+		shopItemsDisplay[num] = ""
+		shopItemsPrice[num] = ""
 	}
 }
 function sell(num){
 	console.log("sell")
-	var item = document.getElementById('playerItems' + num).innerHTML
-	var index = playerItemsDisplay.indexOf(item)
 	
-	playerMoney += parseInt(playerItemsPrice[index])
+	playerMoney += parseInt(player[num].Price)
 	document.getElementById('playerMoney').innerHTML = playerMoney
 	
     document.getElementById('shopItems').innerHTML =
         document.getElementById('shopItems').innerHTML
-        + "<p id='shopItems" + buyNum + "' onclick='buy(" + buyNum + ")'>" + playerItemsDisplay[num] + "</p>";
+        + "<p id='shopItems" + buyNum + "' onclick='buy(" + buyNum + ")'>" + player[num].ItemName + "</p>";
 	
 	document.getElementById('shopItems').innerHTML =
 		document.getElementById('shopItems').innerHTML
-		+ "<p id='shopItems" + buyNum + "a'> ^" + playerItemsPrice[num] + " Gold^</p>";
+		+ "<p id='shopItems" + buyNum + "a'> ^" + player[num].Price + " Gold^</p>";
 	
-	shopItems.push(playerItems[num])
-	shopItemsDisplay.push(playerItemsDisplay[num])
-	shopItemsPrice.push(playerItemsPrice[num])
+	shop.push({})
+	shop[shop.length].ItemID = player[num].ItemID
+	shop[shop.length].ItemName = player[num].ItemName
+	shop[shop.length].Price player[num].Price
 	
 	document.getElementById('playerItems' + num).innerHTML = ""
 	document.getElementById('playerItems' + num + "a").innerHTML = ""
 	buyNum += 1
 	
-	recordTransfer(shopID, playerID, playerItems[num], 1, playerItemsPrice[num])
+	recordTransfer(shopID, playerID, player[num].ItemID, 1, player[num].Price)
 	
-	playerItems[index] = ""
-	playerItemsDisplay[index] = ""
-	playerItemsPrice[index] = ""
+	player[num].ItemID = ""
+	player[num].ItemName = ""
+	player[num].Price = ""
 }
 
